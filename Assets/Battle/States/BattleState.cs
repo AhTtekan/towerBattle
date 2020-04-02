d0f36b26650ef1449b45b7339eabfa608a3ac500
Andrew.Maggert <lacuna.andru.maggert@gmail.com>
2020-04-01 19:00:04 -0500

    moved to swtching InputActionMaps instead of PlayerInputs.

--- Assets/Battle/States/BattleState.cs
+++ Assets/Battle/States/BattleState.cs
@@ -1,11 +1,10 @@
 ﻿using UnityEngine;
-using UnityEngine.InputSystem;
 
 [CreateAssetMenu(fileName = "BattleState", menuName = "ScriptableObjects/Battle", order = 1)]
 public class BattleState : ScriptableObject
 {
     public string stateName;
-    public InputActionAsset playerInput;
+    public string InputMapName;
     public BattleState nextState;
     public BattleState previousState;
 }
--- Assets/Battle/States/BattleStateManager.cs
+++ Assets/Battle/States/BattleStateManager.cs
@@ -1,24 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BattleStateManager : MonoBehaviour
 {
     [SerializeField]
     private BattleState[] battleStates;
+    [SerializeField]
+    private PlayerInput playerInput;
 
     private BattleState currentState;
 
     void Start()
     {
         currentState = battleStates[0];
+        playerInput.SwitchCurrentActionMap(currentState.InputMapName);
+        Debug.Log($"State initialized to {currentState.stateName}");
+        Debug.Log($"Map: {playerInput.currentActionMap.name}");
     }
 
-    public void IncrementBattleState()
+    public void IncrementBattleState(InputAction.CallbackContext callbackContext)
     {
+        if (!callbackContext.performed)
+            return;
         currentState = currentState.nextState;
+        playerInput.SwitchCurrentActionMap(currentState.InputMapName);
+        Debug.Log($"State incremented to {currentState.stateName}");
+        Debug.Log($"Map: {playerInput.currentActionMap.name}");
     }
 
-    public void DecrementBattleState()
+    public void DecrementBattleState(InputAction.CallbackContext callbackContext)
     {
+        if (!callbackContext.performed)
+            return;
         currentState = currentState.previousState;
+        playerInput.SwitchCurrentActionMap(currentState.InputMapName);
+        Debug.Log($"State decremented to {currentState.stateName}");
+        Debug.Log($"Map: {playerInput.currentActionMap.name}");
     }
 }
